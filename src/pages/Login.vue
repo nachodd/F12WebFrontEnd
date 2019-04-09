87fe8da5498945a7fc7f4ee580eb78f03d44789c
nachodd <mdurand@bld.com.ar>
2019-04-09 17:26:44 -0300

    initial commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <div class="q-pa-md row items-start q-gutter-md grey-3">
+        <q-card>
+          <q-card-section>
+            <q-input standout v-model="email" label="Usuario">
+              <template v-slot:prepend>
+                <q-icon name="account_circle" />
+              </template>
+            </q-input>
+
+            <q-input standout v-model="password" label="Password">
+              <template v-slot:prepend>
+                <q-icon name="lock" />
+              </template>
+            </q-input>
+
+
+          </q-card-section>
+        </q-card>
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  }
+};
+</script>
