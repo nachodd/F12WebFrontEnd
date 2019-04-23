81b08e97dbbd880ef9b7b966eae62ffc759acaf3
Ignacio Durand <nachodurand@gmail.com>
2019-04-23 07:31:09 -0300

    changes in widgets

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,11 +4,11 @@
 
     <q-card dark bordered class="bg-grey-9 flex items-center">
       <q-card-section class="flex-column">
-        <q-icon name="fas fa-ticket-alt" size="3rem" />
+        <q-icon name="fas fa-ticket-alt" size="4rem" />
       </q-card-section>
       <q-card-section class="flex-column q-mt-sm">
-        <div class="text-h6">20</div>
-        <div class="text-subtitle2">Tickets nuevos</div>
+        <div class="text-h3">20</div>
+        <div class="text-subtitle">Tickets nuevos</div>
       </q-card-section>
 
       <!-- <q-separator dark inset />
